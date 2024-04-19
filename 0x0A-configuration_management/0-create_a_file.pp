81abadb79bfc50b6392a470b9718519da60a3a09
Obrempong12 <tuntumbrane@gmail.com>
2024-04-19 12:59:01 +0000

    Configuration Management

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,9 +1,9 @@
 # creates a file
 file { 'holberton':
   ensure  => file,
-  path    => '/tmp/holberton',
+  path    => '/tmp/school',
   owner   => 'www-data',
   group   => 'www-data',
-  mode    => '0744',
+  permission    => '0744',
   content => 'I love Puppet'
 }
