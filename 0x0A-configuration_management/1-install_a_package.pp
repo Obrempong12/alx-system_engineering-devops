a88181b6db7d8a90c5f6b5f9790be6976d3bd6f9
Obrempong12 <tuntumbrane@gmail.com>
2024-04-19 13:15:16 +0000

    Configuration Management

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,11 @@
-# a puppet scrip to install puppet-lint
-  package { 'puppet-lint':
-    ensure   => '2.1.0',
-    provider => 'gem',
-  }
+# This manifest installs Flask version 2.1.0
+
+package {'flask':
+  ensure   => '2.1.0',
+  provider => 'pip3',
+}
+
+package {'werkzeug':
+  ensure   => '2.1.1',
+  provider => 'pip3',
+}
